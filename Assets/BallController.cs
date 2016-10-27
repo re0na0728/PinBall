8934e50362a7f6585c76b057bdfbfe3a03b1c6dc
ReonaSuzuki <reona0728@gmail.com>
2016-10-27 20:42:51 +0900

    ポイントの表示

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -9,10 +9,22 @@ public class BallController : MonoBehaviour {
     //ゲームオーバーを表示するテキスト
     private GameObject gameoverText;
 
+    //ポイントを表示するテキスト
+    private GameObject pointText;
+
+    //オブジェクトに衝突加算されるポイント
+    private int point;
+
+    //ポイントの合計値
+    private int sumPoint = 0;
+
 	// Use this for initialization
 	void Start () {
         //シーン中のGameOverTextオブジェクトを取得
         this.gameoverText = GameObject.Find("GameOverText");
+
+        //シーン中のpointTextオブジェクトを取得
+        this.pointText = GameObject.Find("PointText");
 	
 	}
 	
@@ -25,4 +37,29 @@ public class BallController : MonoBehaviour {
         }
 	
 	}
+
+    void OnCollisionEnter(Collision other)
+    {
+        //衝突したオブジェクトによって加算されるポイントを変える
+        if(other.gameObject.tag == "SmallStarTag")
+        {
+            point = 10;
+        }else if(other.gameObject.tag == "LargeStarTag")
+        {
+            point = 20;
+        }else if(other.gameObject.tag == "SmallCloudTag")
+        {
+            point = 30;
+        }else if(other.gameObject.tag == "LargeCloudTag")
+        {
+            point = 40;
+        }
+
+        //ポイントの合計値を加算する
+        sumPoint += point;
+
+        //PointTextにポイントを表示
+        this.pointText.GetComponent<Text>().text = "Point:" + sumPoint;
+
+    }
 }
